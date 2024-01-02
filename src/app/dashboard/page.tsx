6a54f78fc0758350816b975b62e7e0abe3584786
Lucas Nunes Pereira <lucasnp.dev@gmail.com>
2024-01-02 13:45:14 -0300

    sign out

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { signOut } from 'next-auth/react'
+
 export default function Dashboard() {
   return (
     <main>
       <h1>dashboard</h1>
+      <Button onClick={() => signOut()}>Sign Out</Button>
     </main>
   )
 }
